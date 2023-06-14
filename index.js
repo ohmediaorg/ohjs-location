1c76f426563822a4e8d48d841bb1054595c3b3a6
Sean Peterson <seanpeterson.997@gmail.com>
2023-06-14 10:57:59 -0600

    Update index.js
    Refactors

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+export function LatLng(lat, lng) {
+  validateLatLng(lat, lng);
+
+  this.lat = lat;
+  this.lng = lng;
+}
+
 export function getUserPosition() {
   return new Promise((resolve, reject) => {
     if ('geolocation' in navigator) {
@@ -5,7 +12,7 @@ export function getUserPosition() {
         (position) => {
           const userLatitude = position.coords.latitude;
           const userLongitude = position.coords.longitude;
-          resolve({ latitude: userLatitude, longitude: userLongitude });
+          resolve(new LatLng(userLatitude, userLongitude));
         },
         (error) => {
           reject(error);
@@ -17,18 +24,22 @@ export function getUserPosition() {
   });
 }
 
-export function calculateDistance(lat1, lon1, lat2, lon2) {
+export function calculateDistance(latLng1, latLng2) {
+  if (!(latLng1 instanceof LatLng) || !(latLng2 instanceof LatLng)) {
+    throw new Error('Invalid LatLng object');
+  }
+
   const earthRadius = 6371; // Radius of the earth in kilometers
 
-  const dLat = ((lat2 - lat1) * Math.PI) / 180;
-  const dLon = ((lon2 - lon1) * Math.PI) / 180;
+  const dLat = ((latLng2.lat - latLng1.lat) * Math.PI) / 180;
+  const dLon = ((latLng2.lng - latLng1.lng) * Math.PI) / 180;
 
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos((lat1 * Math.PI) / 180) *
-      Math.cos((lat2 * Math.PI) / 180) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
+    Math.cos((latLng1.lat * Math.PI) / 180) *
+    Math.cos((latLng2.lat * Math.PI) / 180) *
+    Math.sin(dLon / 2) *
+    Math.sin(dLon / 2);
 
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   const distance = earthRadius * c;
@@ -36,14 +47,41 @@ export function calculateDistance(lat1, lon1, lat2, lon2) {
   return distance;
 }
 
-export function findNearestLocations(userLat, userLng, locationsArray) {
+export function findNearestLocations(userLatLng, locationsArray, count = 3) {
+  if (!userLatLng instanceof LatLng) {
+    throw new Error('Invalid LatLng object');
+  }
+
   const sortedLocations = locationsArray.sort((a, b) => {
-    const distanceA = calculateDistance(userLat, userLng, a.latitude, a.longitude);
-    const distanceB = calculateDistance(userLat, userLng, b.latitude, b.longitude);
-    return distanceA - distanceB;
+    const latLngA = new LatLng(parseFloat(a.lat), parseFloat(a.lng));
+    const latLngB = new LatLng(parseFloat(b.lat), parseFloat(b.lng));
+
+    return compareDistances(userLatLng, latLngA, latLngB);
   });
 
-  const nearestLocations = sortedLocations.slice(0, 3);
+  const nearestLocations = sortedLocations.slice(0, count);
 
   return nearestLocations;
+}
+
+function compareDistances(userLatLng, a, b) {
+  const distanceA = calculateDistance(userLatLng, a);
+  const distanceB = calculateDistance(userLatLng, b);
+
+  return distanceA - distanceB;
+}
+
+
+function validateLatLng(lat, lng) {
+  if (typeof lat !== 'number' || typeof lng !== 'number') {
+    throw new Error('Invalid latitude or longitude');
+  }
+
+  if (lat < -90 || lat > 90) {
+    throw new Error('Invalid latitude');
+  }
+
+  if (lng < -180 || lng > 180) {
+    throw new Error('Invalid longitude');
+  }
 }
